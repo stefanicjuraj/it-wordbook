fa011a6f1dd2ed3d705405ea4aea640908780f25
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-13 23:46:16 +0100

    feat(app): integrate Search component & data search

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Search from './components/Search.tsx';
 import WordBook from './components/WordBook.tsx';
 import { useData } from './hooks/useData.ts';
 
 const App: React.FC = () => {
   const { data } = useData();
-  return <WordBook data={data} />;
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <>
+      <Search onSearch={(term) => setSearchTerm(term)} />
+      <WordBook data={data} searchTerm={searchTerm} />
+    </>
+  );
 }
 
 export default App
