de3ac846813a48efc55877c7fee1379c601c056a
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-13 20:15:38 +0100

    feat(services): initialize firebase cloud firestore database

--- app/src/services/firebaseService.ts
+++ app/src/services/firebaseService.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -9,4 +10,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
