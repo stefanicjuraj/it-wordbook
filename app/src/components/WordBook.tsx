4a75458ba96189f96fa663aae10eeb68a906db67
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-13 23:40:30 +0100

    feat(components): add data search by word to WordBook component

--- app/src/components/WordBook.tsx
+++ app/src/components/WordBook.tsx
@@ -3,9 +3,13 @@ import { WordBook } from '../types/wordbook.ts';
 
 interface WordBookProps {
     data: WordBook[];
+    searchTerm: string;
 }
 
-const WordBookComponent: React.FC<WordBookProps> = ({ data }) => {
+const WordBookComponent: React.FC<WordBookProps> = ({ data, searchTerm }) => {
+    const filterData = data.filter(item =>
+        item.word.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     return (
         <div className="mt-10 mb-10 max-w-screen-md mx-auto relative overflow-x-auto shadow-md rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500">
@@ -20,7 +24,7 @@ const WordBookComponent: React.FC<WordBookProps> = ({ data }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((item) => (
+                    {filterData.map((item) => (
                         <tr key={item.id} className="border-b border-gray-200 bg-gray-50">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 bg-gray-100">
                                 {item.word}
