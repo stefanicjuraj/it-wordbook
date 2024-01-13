4298198a248f986c66c8843e1ecdff24a1ca102d
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-14 00:15:44 +0100

    feat(components): add data search results count

--- app/src/components/WordBook.tsx
+++ app/src/components/WordBook.tsx
@@ -11,31 +11,36 @@ const WordBookComponent: React.FC<WordBookProps> = ({ data, searchTerm }) => {
         item.word.toLowerCase().includes(searchTerm.toLowerCase())
     );
     return (
-        <div className="mt-10 mb-10 max-w-screen-md mx-auto relative overflow-x-auto shadow-md rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-                <thead className="text-md text-gray-900">
-                    <tr className="border-b border-gray-200">
-                        <th scope="col" className="px-6 py-3 bg-gray-100">
-                            Word
-                        </th>
-                        <th scope="col" className="px-6 py-3 bg-gray-50">
-                            Definition
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filterData.map((item) => (
-                        <tr key={item.id} className="border-b border-gray-200 bg-gray-50">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 bg-gray-100">
-                                {item.word}
+        <div className="max-w-screen-md mx-auto my-10">
+            <p className="m-2 text-gray-400 text-sm text-right">
+                {filterData.length} result(s)
+            </p>
+            <div className="relative overflow-x-auto shadow-md rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                    <thead className="text-md text-gray-900">
+                        <tr className="border-b border-gray-200">
+                            <th scope="col" className="px-6 py-3 bg-gray-100">
+                                Word
                             </th>
-                            <td className="px-6 py-4 text-gray-700">
-                                {item.definition}
-                            </td>
+                            <th scope="col" className="px-6 py-3 bg-gray-50">
+                                Definition
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {filterData.map((item) => (
+                            <tr key={item.id} className="border-b border-gray-200 bg-gray-50">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 bg-gray-100">
+                                    {item.word}
+                                </th>
+                                <td className="px-6 py-4 text-gray-700">
+                                    {item.definition}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
