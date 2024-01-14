d6a64286a33fc24fb2c68ddedbde0271d21eddb5
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-15 00:49:17 +0100

    feat(components): add Tags component

---
+++ app/src/components/Tags.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface TagsProps {
+    tags: string[];
+}
+
+const Tags: React.FC<TagsProps> = ({ tags }) => {
+    const renderTags = () => {
+        const rows: JSX.Element[] = [];
+        for (let i = 0; i < tags.length; i += 2) {
+            rows.push(
+                <div key={i} className="flex">
+                    {tags.slice(i, i + 2).map((tag, index) => (
+                        <span key={index} className="bg-gray-200 text-xs text-gray-500 font-normal px-1.5 py-1 rounded-md mx-0.5 my-0.5">
+                            {tag}
+                        </span>
+                    ))}
+                </div>
+            );
+        }
+        return rows;
+    };
+
+    return (
+        <>
+            {renderTags()}
+        </>
+    );
+};
+
+export default Tags;
