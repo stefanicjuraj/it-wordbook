ac1b96dab9988181b32a89b00373c2442d81062c
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-15 01:24:54 +0100

    feat(components): sort tags alphabetically

--- app/src/components/Tags.tsx
+++ app/src/components/Tags.tsx
@@ -5,12 +5,14 @@ interface TagsProps {
 }
 
 const Tags: React.FC<TagsProps> = ({ tags }) => {
+    const sortTags = [...tags].sort();
+
     const renderTags = () => {
         const rows: JSX.Element[] = [];
-        for (let i = 0; i < tags.length; i += 2) {
+        for (let i = 0; i < sortTags.length; i += 2) {
             rows.push(
                 <div key={i} className="flex">
-                    {tags.slice(i, i + 2).map((tag, index) => (
+                    {sortTags.slice(i, i + 2).map((tag, index) => (
                         <span key={index} className="bg-gray-200 text-xs text-gray-500 font-normal px-1.5 py-1 rounded-md mx-0.5 my-0.5">
                             {tag}
                         </span>
