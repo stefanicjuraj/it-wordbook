ee012e55b1ff22f5e30aee5813db7f3e52fcdb20
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-16 00:28:42 +0100

    feat(app): integrate FilterTags component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,16 +3,25 @@ import Header from './components/Header.tsx';
 import Search from './components/Search.tsx';
 import WordBook from './components/WordBook.tsx';
 import { useData } from './hooks/useData.ts';
+import FilterTags from './components/FilterTags.tsx';
 
 const App: React.FC = () => {
-  const { data } = useData();
+  const { data, tags } = useData();
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const filterDataByTags = data.filter(item => {
+    if (selectedTags.length === 0) return true;
+    return selectedTags.every(tag => item.tags.includes(tag)) &&
+      item.tags.every(tag => selectedTags.includes(tag));
+  });
 
   return (
     <>
       <Header />
       <Search onSearch={(term) => setSearchTerm(term)} />
-      <WordBook data={data} searchTerm={searchTerm} />
+      <FilterTags tags={tags} onTagsChange={setSelectedTags} />
+      <WordBook data={filterDataByTags} searchTerm={searchTerm} />
     </>
   );
 }
