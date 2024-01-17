d750c52d031ad470010d3ca04ffcbf0336e0cafe
stefanicjuraj <jurajstefanic@outlook.com>
2024-01-17 22:28:15 +0100

    feat(components): add tag filter paragraph

--- app/src/components/FilterTags.tsx
+++ app/src/components/FilterTags.tsx
@@ -30,30 +30,30 @@ const FilterTags: React.FC<FilterTags> = ({ tags, onTagsChange }) => {
 
     return (
         <div className="max-w-screen-sm mx-auto mt-10">
-            {selectedTags.length > 0 && (
-                <button type="button" className="text-xs font-normal text-center text-gray-500"
+            <p className="text-gray-400 text-sm text-center my-2">
+                Apply a tag to filter your search
+            </p>
+            <ul className="flex flex-wrap mx-auto items-center w-full rounded-lg justify-center">
+                {sortTags.map(tag => (
+                    <li key={tag} className="w-1/3 md:w-1/3 lg:w-1/5 mt-2 bg-gray-100 mx-1 p-3 rounded-xl">
+                        <input type="checkbox"
+                            id={`tag-${tag}`}
+                            name={`tag-${tag}`}
+                            value={tag}
+                            checked={selectedTags.includes(tag)}
+                            onChange={() => handleCheckboxChange(tag)}
+                            className="w-5 h-5 ml-1 text-black bg-gray-100 border-gray-400 rounded focus:ring-gray-900" />
+                        <label htmlFor={`tag-${tag}`} className="w-full ml-2 text-sm font-normal text-gray-900">
+                            {tag}
+                        </label>
+                    </li>
+                ))}
+            </ul>
+            {selectedTags.length > 1 && (
+                <button type="button" className="w-1/4 mt-4 text-xs font-normal text-center text-gray-500"
                     onClick={clearTags}>Clear tags</button>
             )}
-            <ul className="items-center w-full bg-white rounded-lg sm:flex">
-                {sortTags.map(tag => (
-                    <li key={tag} className="w-full">
-                        <div className="flex items-center ps-3">
-                            <input type="checkbox"
-                                id={`tag-${tag}`}
-                                name={`tag-${tag}`}
-                                value={tag}
-                                checked={selectedTags.includes(tag)}
-                                onChange={() => handleCheckboxChange(tag)}
-                                className="w-4 h-4 text-black bg-gray-100 border-gray-300 rounded focus:ring-gray-900" />
-                            <label htmlFor={`tag-${tag}`} className="w-full py-3 ms-2 text-sm font-normal text-gray-900">
-                                {tag}
-                            </label>
-                        </div>
-                    </li>
-                ))
-                }
-            </ul>
-        </div >
+        </div>
     );
 };
 
