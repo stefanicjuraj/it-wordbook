2d45055b30206d9ae173d12331c6314d671fb452
stefanicjuraj <jurajstefanic@outlook.com>
2024-02-01 23:30:05 +0100

    feat(components): add search icon to Search

--- app/src/components/Search.tsx
+++ app/src/components/Search.tsx
@@ -1,7 +1,12 @@
+import search from "../../public/assets/icons/search.svg"
+
 const Search: React.FC<{ onSearch: (term: string) => void }> = ({ onSearch }) => {
     return (
         <form className="max-w-screen-sm mx-auto">
             <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <img src={search} alt="Search icon" className="w-5 h-5" />
+                </div>
                 <input type="search" id="search" className="block w-full p-4 text-sm text-gray-900 border border-gray-300 ps-10 rounded-xl bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
                     placeholder="Search IT keywords, terms, principles, and concepts"
                     autoComplete="off"
