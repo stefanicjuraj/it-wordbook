944e7a0049d2a1b68cfe242e694a3b4df3b58822
stefanicjuraj <jurajstefanic@outlook.com>
2024-02-03 23:13:02 +0100

    feat(components): add max length to Search input

--- app/src/components/Search.tsx
+++ app/src/components/Search.tsx
@@ -8,6 +8,7 @@ const Search: React.FC<{ onSearch: (term: string) => void }> = ({ onSearch }) =>
                     <img src={search} alt="Search icon" className="w-5 h-5" />
                 </div>
                 <input type="search" id="search" className="block w-full p-4 text-sm text-gray-900 border border-gray-300 ps-10 rounded-xl bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+                    maxLength={50}
                     placeholder="Search IT keywords, terms, principles, and concepts"
                     autoComplete="off"
                     onChange={(e) => onSearch(e.target.value)}
