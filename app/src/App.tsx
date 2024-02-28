6aa9ef9349d48866d72e2c428ddda6c77bce7973
stefanicjuraj <jurajstefanic@outlook.com>
2024-02-28 23:16:37 +0100

    refactor(app): integrate renamed WordBook component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.tsx';
 import Search from './components/Search.tsx';
-import WordBook from './components/WordBook.tsx';
+import { WordBook } from './components/WordBook.tsx';
 import { useData } from './hooks/useData.ts';
 import FilterTags from './components/FilterTags.tsx';
 import ScrollToTop from './components/ScrollToTop.tsx';
