9c116482baeafbddcbf684100748fe44ce499665
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-17 19:01:37 +0100

    chore: modify heading capitalization

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
               alt="logo"
               className="w-10 h-10 mr-3"
             />
-            <h1 className="text-2xl font-bold text-white">IT Wordbook</h1>
+            <h1 className="text-2xl font-bold text-white">IT wordbook</h1>
           </div>
 
           <div className="flex items-center">
