75ca21854733174d85e15378fae364581f73cab3
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-18 08:02:53 +0100

    feat: add cloudflare script

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "../globals.css";
 
 export const metadata: Metadata = {
@@ -52,6 +53,11 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/assets/icons/techbook-logo.svg" />
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "782d4072fcd24bb984d9bf8363cdf612"}'
+        />
       </head>
       <body>{children}</body>
     </html>
