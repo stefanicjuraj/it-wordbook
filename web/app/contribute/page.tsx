e26c7d0f3c2004472afafaafa725eec9b10d9531
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-18 18:13:13 +0100

    feat: add contribute page

---
+++ web/app/contribute/page.tsx
@@ -0,0 +1,11 @@
+import Contribute from "../../components/Contribute";
+import Navbar from "../../components/Navbar";
+
+export default function ContributePage() {
+  return (
+    <>
+      <Navbar />
+      <Contribute />
+    </>
+  );
+}
